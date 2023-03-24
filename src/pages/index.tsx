7db34ae55461676f893a57680c8f43a95546bf0e
Anthony Labadie <anthonylabadie@MacBook-Pro-de-Anthony.local>
2023-03-24 09:39:43 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+import { getSortedPostsData } from '../../lib/posts';
+import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+
+export default function Home({ allPostsData }: any) {
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <section>
+        <h2>Blog</h2>
+      </section>()
+      {allPostsData.map((post: any) => (
+        <Link href={`/posts/${post.id}`}>
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,37 @@
+
+import Date from '@/components/Date';
+import Head from 'next/head';
+import { getAllPostIds, getPostData } from '../../../lib/posts';
+
+export default function Post({ postData }: any) {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </div>
+  );
+}
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export async function getStaticProps({ params }: any) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
